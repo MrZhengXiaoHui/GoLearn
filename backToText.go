e99bf03cb2bc2b425b8248e9c725d8169c36c166
xiaohui <xiaohui@warriormedia.cn>
2019-10-31 15:13:08 +0800

    素数、乘法表、回文判断和字符统计

---
+++ backToText.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func BackToText(str string) {
+	//编写一个函数接受一个字符串参数，输出该字符串是否是回文（从前往后和从后往前都是一样的字符串成为回文）
+	str1 := ""
+	for i := len(str) - 1; i >= 0; i-- {
+		str1 += string(str[i])
+	}
+	fmt.Println(str == str1)
+}
+
+//func main() {
+//	BackToText("abccba") //true
+//	BackToText("abcbac") //false
+//}
---
+++ charStatistics.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func CharacterStatistics(str string) {
+	//编写一个函数接收一个字符串参数，输出该字符串中包含的中文字符数，英文字符数，数字字符数还有其他字符数。
+	quantity := map[string]int{
+		"chineseQuantity": 0,
+		"englishQuantity": 0,
+		"numberQuantity":  0,
+		"otherQuantity":   0,
+	}
+	for _, v := range str {
+		if regexp.MustCompile("^[\u4e00-\u9fa5]$").MatchString(string(v)) {
+			quantity["chineseQuantity"]++
+			continue
+		}
+		if v >= 'a' && v <= 'z' || v >= 'A' && v <= 'Z' {
+			quantity["englishQuantity"]++
+			continue
+		}
+		if v >= '0' && v <= '9' {
+			quantity["numberQuantity"]++
+			continue
+		}
+		quantity["otherQuantity"]++
+	}
+	fmt.Println(quantity)
+}
+
+//func main() {
+//	CharacterStatistics("11中1aaa一bb-   ")
+//}
